12f3bf21730a626b4a8814291e1549c9046b53f4
parkcai <162147799+parkcai@users.noreply.github.com>
2025-07-20 11:33:08 +0800

    更新版本号至 0.3.8.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name = "pywheels",
-    version = "0.3.8",
+    version = "0.3.8.1",
     packages = find_packages(),
     description = "Light-weight Python wheels",
     author = "parkcai",
