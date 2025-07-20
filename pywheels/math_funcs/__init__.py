cba7a855eaecbdaa098d0f158632ae80e14b6859
parkcai <162147799+parkcai@users.noreply.github.com>
2025-07-20 11:31:07 +0800

    新增统计函数 chi_squared 和 mean_squared_error，并更新版本号至 0.3.8

---
+++ pywheels/math_funcs/__init__.py
@@ -0,0 +1,8 @@
+from .stat import chi_squared
+from .stat import mean_squared_error
+
+
+__all__ = [
+    "chi_squared",
+    "mean_squared_error",
+]
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name = "pywheels",
-    version = "0.3.7",
+    version = "0.3.8",
     packages = find_packages(),
     description = "Light-weight Python wheels",
     author = "parkcai",
